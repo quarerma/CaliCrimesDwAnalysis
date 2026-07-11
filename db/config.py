2021579874e298023ed338c857c050508d7720e7
quarerma <gabriel.oliveira.quaresma@gmail.com>
2026-07-11 14:54:19 -0300

    change default port

--- db/config.py
+++ db/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_PORT = os.getenv("DB_PORT", "5433")
 DB_NAME = os.getenv("DB_NAME", "oltp")
 DB_USER = os.getenv("DB_USER", "oltp")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "oltp")
